253d0d3198d84dfcf9ae80edd176e854eb63f5a5
C70Schwager <cyrillgoesweblop@protonmail.com>
2023-08-30 08:43:17 +0200

    los gehts

--- includes/db.php
+++ includes/db.php
@@ -5,7 +5,7 @@ $dbName = getenv('DB_NAME');
 $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 
-$dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", '$dbUser', '$dbPassword');
+$dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", '$dbUser.php', '$dbPassword.php');
 
 // Setze den Fehlermodus für Web Devs
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Trivial-Quiz</title>
-        <link rel="stylesheet" href="styles.css"/>
+        <link rel="stylesheet" href="css/header.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="js/main.js"></script>
